3a7b3cf8d6c38187f2cf480b21c00813eafc4245
SaraGri <66334860+SaraGri@users.noreply.github.com>
2020-08-04 00:11:08 +0200

    assets

---
+++ Assets/ScanManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class ScanManager : MonoBehaviour, IInputClickHandler
+{
+    public TextMesh InstructionTextMesh;
+    public Transform FloorPrefab;
+    public TestExporter exporter;
+    bool once = true;
+    // Use this for initialization
+    void Start()
+    {
+        //SpatialUnderstanding.Instance.UnderstandingSourceMesh.
+        InputManager.Instance.PushFallbackInputHandler(this.gameObject);
+        SpatialUnderstanding.Instance.RequestBeginScanning();
+        SpatialUnderstanding.Instance.ScanStateChanged += ScanStateChanged;
+    }
+
+    private void ScanStateChanged()
+    {
+        if (SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Scanning)
+        {
+            LogSurfaceState();
+        }
+        else if (SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Done)
+        {
+            //exporter.Export("test.obj"); ; // InstanciateObjectOnFloor();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SpatialUnderstanding.Instance.ScanStateChanged -= ScanStateChanged;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (SpatialUnderstanding.Instance.ScanState)
+        {
+            case SpatialUnderstanding.ScanStates.None:
+                break;
+            case SpatialUnderstanding.ScanStates.ReadyToScan:
+                break;
+            case SpatialUnderstanding.ScanStates.Scanning:
+                this.LogSurfaceState();
+                break;
+            case SpatialUnderstanding.ScanStates.Finishing:
+                this.InstructionTextMesh.text = "State: Finishing Scan";
+                //if(once) {
+                //    exporter.Export("test.obj");
+                //    once = false;
+                //}
+                break;
+            case SpatialUnderstanding.ScanStates.Done:
+                this.InstructionTextMesh.text = "State: Scan DONE";
+                //this.LogSurfaceState();
+//#if WINDOWS_UWP
+  //              Windows.StorageFolder storageFolder = KnownFolders.Object3D;
+    //            StorageFile file = await storageFolder.CreateFileAsync("sample.png", CreationCollisionOption.ReplaceExisting);
+//#endif
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void LogSurfaceState()
+    {
+        IntPtr statsPtr = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStatsPtr();
+        if (SpatialUnderstandingDll.Imports.QueryPlayspaceStats(statsPtr) != 0)
+        {
+            var stats = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStats();
+            this.InstructionTextMesh.text = string.Format("TotalSurfaceArea: {0:0.##} - WallSurfaceArea: {1:0.##} - HorizSurfaceArea: {2:0.##}", stats.TotalSurfaceArea, stats.WallSurfaceArea, stats.HorizSurfaceArea);
+
+            //var rayResult = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticRaycastResult();
+            //this.InstructionTextMesh.text = rayResult.SurfaceType.ToString();
+        }
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        this.InstructionTextMesh.text = "Requested Finish Scan";
+        //Debug.Log(eventData.selectedObject);
+
+        //SpatialUnderstanding.Instance.RequestFinishScan();
+    }
+
+    private void InstanciateObjectOnFloor()
+    {
+        const int QueryResultMaxCount = 512;
+
+        SpatialUnderstandingDllTopology.TopologyResult[] _resultsTopology = new SpatialUnderstandingDllTopology.TopologyResult[QueryResultMaxCount];
+
+        var minLengthFloorSpace = 0.25f;
+        var minWidthFloorSpace = 0.25f;
+
+        var resultsTopologyPtr = SpatialUnderstanding.Instance.UnderstandingDLL.PinObject(_resultsTopology);
+        var locationCount = SpatialUnderstandingDllTopology.QueryTopology_FindPositionsOnFloor(minLengthFloorSpace, minWidthFloorSpace, _resultsTopology.Length, resultsTopologyPtr);
+
+        if (locationCount > 0)
+        {
+            Instantiate(this.FloorPrefab, _resultsTopology[0].position, Quaternion.LookRotation(_resultsTopology[0].normal, Vector3.up));
+
+            this.InstructionTextMesh.text = "Placed the hologram";
+        }
+        else
+        {
+            this.InstructionTextMesh.text = "I can't found the enough space to place the hologram.";
+        }
+    }
+
+    public void Test() { }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class UIController : MonoBehaviour {
+    // Ref to spatial mapping
+    public GameObject spatialMapping;
+    private SpatialMappingManager spatialMappingManager;
+
+    // Ref to spatial understanding
+    public GameObject spatialUnderstanding;
+    private SpatialUnderstandingCustomMesh spatialUnderstandingCustomMesh;
+
+    // Ref to scan manager (management of spatial understanding)
+    public GameObject mappingOrchestrator;
+    private ScanManager scanManager;
+    private SpatialMappingObserver surfaceObserver;
+    
+    // public bool drawSpatialMapping;
+    //public bool drawSpatialUnderstanding;
+
+    // Use this for initialization
+    void Start() {
+        // Init ref to spatial mapping
+        spatialMappingManager = spatialMapping.GetComponent<SpatialMappingManager>();
+
+        // Init ref to spatial understanding
+        spatialUnderstandingCustomMesh = spatialUnderstanding.GetComponent<SpatialUnderstandingCustomMesh>();
+
+        // Init ref to scan manager (management of spatial understanding)
+        scanManager = mappingOrchestrator.GetComponent<ScanManager>();
+        spatialMappingManager.DrawVisualMeshes = false;
+        //drawSpatialMapping = false;
+        //drawSpatialUnderstanding = false;
+        surfaceObserver = gameObject.GetComponent<SpatialMappingObserver>();
+
+
+
+
+
+        //Änderungen von mir
+        manager = spatialMapping.GetComponent<SpatialMappingManager>();
+
+    }
+
+    
+
+
+
+    //Meins
+    //Für das Einschalten des Observers, damit Netz nicht automatisch starten
+    public SpatialMappingManager manager;
+    //Obseverobjekt
+    //private SpatialMappingObserver surfaceObserver2;
+   // public SpatialMappingObserver SurfaceObserver { get { return surfaceObserver2; } }
+
+
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+   
+
+    public void HandleSpatialMapping (bool drawSpatialMapping) {
+         
+        if (drawSpatialMapping) {
+
+            
+            spatialMappingManager.DrawVisualMeshes = true;
+            spatialMappingManager.StartObserver();
+           
+
+    // drawSpatialMapping = false;
+} else {
+            spatialMappingManager.DrawVisualMeshes = false;
+           // drawSpatialMapping = true;
+        }
+    }
+
+   
+
+    public void HandleSpatialUnderstanding (bool drawSpatialUnderstanding) {
+        if (drawSpatialUnderstanding) {
+            spatialUnderstandingCustomMesh.DrawProcessedMesh = true;
+           // drawSpatialUnderstanding = false;
+        } else {
+            spatialUnderstandingCustomMesh.DrawProcessedMesh = false;
+         //   drawSpatialUnderstanding = true;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+    //Methoden von mir
+
+    public void SpatialMappingStarten_Stoppen(bool starten)
+    {
+        //Mesh visuell anzeigen lassen
+        spatialMappingManager.DrawVisualMeshes = true;
+
+        if (starten)
+        {
+           
+            //MappingObserver starten
+            spatialMappingManager.StartObserver();
+        }
+       // Der SurfaceObserver wird angewiesen das Updating vom SpatialMapping mesh zu unterlassen 
+        else
+        {
+            spatialMappingManager.StopObserver();
+        }
+    }
+
+    //Befielt den SurfaceObserver zu stoppen und alle meshes zu cleanen
+    public void AnwendungBeenden()
+    {
+        spatialMappingManager.CleanupObserver();
+    }
+    
+}
+
+
+
+
